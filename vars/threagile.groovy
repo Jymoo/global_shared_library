80a3f269f4e71e724a5dd0591b87847c69cabd80
Livingstone <95464726+Jymoo@users.noreply.github.com>
2024-12-27 08:43:39 +0300

    Update threagile.groovy

--- vars/threagile.groovy
+++ vars/threagile.groovy
@@ -25,52 +25,13 @@ def call(Map params = [:]) {
         sh "docker pull ${dockerImage}"
     }
 
-   // Run the Docker container with the provided command  docker run --rm -v \$(pwd):/app/work ${dockerImage} -verbose -model /app/work/${threagileYamlPath} -output /app/work/${outputDir}  docker run --rm -v \$(pwd):/app/work ${dockerImage} -verbose -model /app/work/${threagileYamlPath} -output /app/work/${outputDir}        docker run --rm -v /tmp:/app/work ${dockerImage} -verbose -model /app/work/${threagileYamlPath} -output /app/work/${outputDir}   docker run --rm -v /tmp:/app/work ${dockerImage} -verbose -model /app/work/${threagileYamlPath} -output /app/work/${outputDir}
-    // echo "Running Docker container..."
-    // sh """
-    //    docker run --rm -v \$(pwd):/app/work ${dockerImage} -verbose -model /app/work/${threagileYamlPath} -output /app/work/${outputDir} 
-    // """
+//   Run the Docker container with the provided command  docker run --rm -v \$(pwd):/app/work ${dockerImage} -verbose -model /app/work/${threagileYamlPath} -output /app/work/${outputDir}  docker run --rm -v \$(pwd):/app/work ${dockerImage} -verbose -model /app/work/${threagileYamlPath} -output /app/work/${outputDir}        docker run --rm -v /tmp:/app/work ${dockerImage} -verbose -model /app/work/${threagileYamlPath} -output /app/work/${outputDir}   docker run --rm -v /tmp:/app/work ${dockerImage} -verbose -model /app/work/${threagileYamlPath} -output /app/work/${outputDir}
+    echo "Running Docker container..."
+    sh """
+       docker run --rm -v \$(pwd):/app/work ${dockerImage} -verbose -model /app/work/${threagileYamlPath} -output /app/work/${outputDir} 
+    """
 
-
-    
-    
-    // Define the new workspace path within the user's home directory
-    def newWorkspacePath = "~/threagile_workspaces"
-    
-    // Create the new workspace directory if it doesn't exist
-    new File(newWorkspacePath).mkdirs()
-    
-    // Move the existing workspace to the new location
-    sh "mv ${tempWorkspace} ${newWorkspacePath}/threagile_workspace"
-    
-    // Update the tempWorkspace variable to reflect the new location
-    tempWorkspace = "${newWorkspacePath}/threagile_workspace"
-    
-    // Create the temporary workspace directory if it doesn't exist
-    new File(tempWorkspace).mkdirs()
-    
-    // Copy the threagile.yaml file to the temporary workspace
-    def fileName = new File(threagileYamlPath).name
-    sh "cp ${threagileYamlPath} ${tempWorkspace}/${fileName}"
-    
-    // Change the current working directory to the temporary workspace
-    dir(tempWorkspace) {
-        // Run the Docker container with updated paths and volumes
-        sh """
-        docker run --rm \
-            -v ${tempWorkspace}:/app/work \
-            -v /home/jenkins/workspace/devsecops:/app \
-            --userns=host \
-            ${dockerImage} \
-            -verbose -model /app/work/${fileName} -output /app/work/${outputDir}
-        """
-    }
-        
-
-
-
-
-    
+   
     
     // After running the container, check if the report.pdf file exists in the output directory
     def reportPath = "${PWD}/${outputDir}/${reportFile}"
